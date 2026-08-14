7eb5b1d5c30695b4fb9c8f4309058d4f7469d8d1
Ujaan Das <ujaandas03@gmail.com>
2026-08-14 09:32:45 +0800

    add markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), markdoc()],
 });
